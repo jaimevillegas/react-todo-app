f74d6bd56ab5141a171e7de8581f4e09d7dcb798
Jaime Villegas <jaimevillegas296@gmail.com>
2023-03-27 15:13:56 -0500

    Work on linter errors

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -4,7 +4,7 @@ import TodoItem from './TodoItem';
 const TodosList = ({ todosProps, handleChange, delTodo }) => (
   <ul>
     {todosProps.map((todo) => (
-      <TodoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo}/>
+      <TodoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo} />
     ))}
   </ul>
 );
