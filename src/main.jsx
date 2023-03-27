8c356ed0298ceb0cca0c2e4f31339737e147531f
Jaime Villegas <jaimevillegas296@gmail.com>
2023-03-27 10:23:31 -0500

    Linters config

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import TodoApp from "./components/TodoApp";
+import TodoApp from './components/TodoApp';
 
-const domContainer = document.getElementById("root");
+const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(<TodoApp />);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': Path2D.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 });
